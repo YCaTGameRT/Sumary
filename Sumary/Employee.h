d2300dca813edfe7a153203f28f3febc868ddeac
YCaTGameRT <ycatgamert@gmail.com>
2026-04-02 00:15:42 +0500

    wow

--- Sumary/Employee.h
+++ Sumary/Employee.h
@@ -1,4 +1,9 @@
-#pragma once
+#ifndef _EMPLOYEE_H_
+#define _EMPLOYEE_H_
+
+#include <iostream>
+#include <string>
+
 class Employee {
 public:
 	virtual double Earnings() const = 0;
@@ -7,3 +12,4 @@ private:
 	std::string name;
 };
 
+#endif
